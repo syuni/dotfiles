2668862ca25becedb8915c18e300b4a9ecb65d47
syuni <motikix@gmail.com>
2019-04-11 02:00:16 +0900

    Change hyper.js theme

--- .hyper.js
+++ .hyper.js
@@ -142,7 +142,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-statusline', "hyper-one-dark"],
+  plugins: ['hyper-statusline', "hyper-one-light"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
