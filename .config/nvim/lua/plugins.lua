0faacf14203f2f2ac835615e7edcda6988c483e2
syuni <motikix@gmail.com>
2021-07-15 18:12:32 +0900

    fix indent-blankline context patterns

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -230,7 +230,18 @@ require('packer').startup(function()
       vim.g.indent_blankline_use_treesitter = true
       vim.g.indent_blankline_show_trailing_blankline_indent = false
       vim.g.indent_blankline_show_current_context = true
-      vim.g.indent_blankline_context_patterns = { 'class', 'function', 'method', '^if', '^while', '^for', '^object', '^table', 'block', 'arguments', 'element' }
+      vim.g.indent_blankline_context_patterns = {
+        'class', 'function', 'method', '^if', '^while', '^for', '^object', '^table', 'block', 'arguments',
+        -- HTML
+        'element',
+        -- Pytnon
+        'dictionary',
+        'set',
+        'list',
+        'tuple',
+        -- JavaScript
+        'array',
+      }
     end,
   }
   use {
